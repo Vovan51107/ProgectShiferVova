1eb2b0bd33cee02f5fe68f5719c2a5860136c19f
Vovan51107 <Vladimir275AB@gmail.com>
2022-06-11 12:18:55 +0300

    может поможет

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def kriptografiruy(s, k):
     for i in s:
         if i.lower() != i:
             i = zagl.index(i) + k
-            if i >= 27:
-                l = (i - 27) // 27
+            if i >= 25:
+                l = (i - 25) // 25
                 if l == 0:
                     l += 1
                 for k in range(l-1):
@@ -14,15 +14,11 @@ def kriptografiruy(s, k):
             s_new = s_new + zagl[i]
         else:
             i = stroch.index(i) + k
-            if i >= 27:    
-                l = (i - 27) // 27
-                if l == 0:
-                    l += 1
+            if i >= 25:    
+                l = (i - 25) // 25 + 1
                 for k in range(l-1):
                     stroch = stroch + stroch
             s_new = s_new + stroch[i]
     return s_new
 
 print(kriptografiruy(input(), int(input())))
-
-print('ПРивет')
